ea959a78e07e49aa838b5c01084fd281df0f1bec
Ederson Cruz <edersoncruz1337@gmail.com>
2025-05-17 18:48:00 -0300

    Atualizacao aula: aula 17052025

--- ExercicioAula62.py
+++ ExercicioAula62.py
@@ -1,52 +1,58 @@
 import random
 
-numeros_aleatorios = [random.randint(0, 10) for _ in range(9)]  # Gera 9 números entre 0 e 10
-print(numeros_aleatorios)
+cpf_nove_digitos = [random.randint(0, 9) for _ in range(9)]  # Gera 9 números entre 0 e 10
+# cpf_nove_digitos = [6,1,1,0,3,8,3,6,9]
 
 
-cpf_cru = numeros_aleatorios
+# Primeiro Digito
 
-##Primeiro Digito
-
-cpf_nove_primeiros = cpf_cru[:9]
 valor_1 = 0
 indice_reverso_1 = 10
 
-for numero in cpf_nove_primeiros:
+for numero in cpf_nove_digitos:
         numero_int = int(numero)
         valor_1 += numero_int * indice_reverso_1
         indice_reverso_1 -= 1 
 
-resultado = (valor_1 * 10) % 11
+resultado_1 = (valor_1 * 10) % 11
 
-if resultado > 9:
+if resultado_1 > 9:
     primeiro_digito = 0
 else:
-    primeiro_digito = resultado
+    primeiro_digito = resultado_1
 
-print(f'O seu primeiro dígito é: {primeiro_digito}')
+# print(f'O seu primeiro dígito é: {primeiro_digito}')
 
-##Segundo Dígito
+#Segundo Dígito
 
-cpf_dez_primeiros = cpf_cru[:10]
+cpf_nove_digitos.append(primeiro_digito)
+cpf_dez_digitos = cpf_nove_digitos
+
 valor_2 = 0
 indice_reverso_2 = 11
 
-for numero in cpf_dez_primeiros:
+for numero in cpf_dez_digitos:
         numero_int = int(numero)
         valor_2 += numero_int * indice_reverso_2
         indice_reverso_2 -= 1 
 
-resultado = (valor_2 * 10) % 11
+resultado_2 = (valor_2 * 10) % 11
 
-if resultado > 9:
+if resultado_2 > 9:
     segundo_digito = 0
 else:
-    segundo_digito = resultado
+    segundo_digito = resultado_2
 
-print(f'O seu segundo dígito é: {segundo_digito}')
 
-#Validar CPF
+# Juntar CPF + Digitos
 
-cpf_digitos = str(cpf_cru) + str(primeiro_digito) + str(segundo_digito)
-print (f"Seu CPF: {cpf_digitos}")
+cpf_dez_digitos.append(segundo_digito)
+cpf_onze_digitos = "".join(map(str, cpf_dez_digitos))
+
+
+print(f'CPF Válido: {cpf_onze_digitos}')
+
+
+
+
+
