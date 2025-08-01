7af33e2363e7d30a167cde1282c39a583fae2407
Ederson Cruz <edersoncruz1337@gmail.com>
2025-08-01 19:08:39 -0300

    Hello world

--- aula206_django/project/urls.py
+++ aula206_django/project/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    print("This is my view")
+    return HttpResponse("Hello, world! This is my view.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
